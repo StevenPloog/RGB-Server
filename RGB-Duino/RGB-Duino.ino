f9d9b9d4b680145806cdec4c3aec9bb32e9ebd27
Steven Ploog <emailstevenp@gmail.com>
2016-07-06 16:08:49 -0500

    Setup softSerial and basic test

--- RGB-Duino/RGB-Duino.ino
+++ RGB-Duino/RGB-Duino.ino
@@ -1,9 +1,38 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SoftwareSerial.h>
 
+#define PIN_RED   3
+#define PIN_GREEN 5
+#define PIN_BLUE  6
+#define PIN_RX    10
+#define PIN_TX    11
+
+SoftwareSerial softSerial(PIN_RX, PIN_TX);
+char last_char;
+
+void setup() {
+  pinMode(PIN_RED, OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
+  pinMode(PIN_BLUE, OUTPUT);
+  
+  pinMode(PIN_RX, INPUT);
+  pinMode(PIN_TX, OUTPUT);
+
+  softSerial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (softSerial.available() > 0) {
+    last_char = softSerial.read();
+    Serial.write(last_char);
+    if (last_char == 'X') {
+      analogWrite(PIN_RED, 0);
+      analogWrite(PIN_GREEN, 0);
+      analogWrite(PIN_BLUE, 255);
+    } else {
+      analogWrite(PIN_RED, 0);
+      analogWrite(PIN_GREEN, 0);
+      analogWrite(PIN_BLUE, 100);
+    }
+  }
 }
