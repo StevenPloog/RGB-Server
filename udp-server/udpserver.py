522d086a4c22111a8f3e931f131670abfd911a2b
stevenploog <emailstevenp@gmail.com>
2016-11-07 04:17:16 +0000

    Finalize for real

--- udp-server/udpserver.py
+++ udp-server/udpserver.py
@@ -11,9 +11,12 @@ sock.bind(('', UDP_PORT))
 
 while True:
     message, address = sock.recvfrom(1024)
+    if len(message) > 18:
+        continue
     message = message.decode('utf-8')
     msg = message.split(',')
     if msg[0] == MSG_START and len(msg) == 5 and msg[4] == MSG_END:
         rgb_args = str(msg[1]) + ',' + str(msg[2]) + ',' + str(msg[3]) + ','
         args_str = '1,' + '1,' + rgb_args
         call(['/home/steven/uart', str(2+len(args_str)), args_str])
+
