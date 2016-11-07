a021502c99fa9362bea25e89009f0f5f2e3cb321
Steven Ploog <emailstevenp@gmail.com>
2016-11-06 20:18:07 -0600

    Debug

--- udp-server/udpserver.py
+++ udp-server/udpserver.py
@@ -2,8 +2,8 @@ from subprocess import call
 import socket
 
 UDP_PORT = 1738
-MSG_START = 0xAB
-MSG_END = 0xCD
+MSG_START = 'AB'
+MSG_END = 'CD'
 
 def run_server():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -13,7 +13,7 @@ def run_server():
         message, address = sock.recvfrom(1024)
         message = message.decode('utf-8')
         msg = message.split(',')
-        if int(msg[0]) == MSG_START and len(msg) == 5 and int(msg[4]) == MSG_END:
+        if msg[0] == MSG_START and len(msg) == 5 and msg[4] == MSG_END:
             rgb_args = str(msg[1]) + ',' + str(msg[2]) + ',' + str(msg[3]) + ','
             args_str = '1,' + '1,' + rgb_args
             #call(['/var/www/mysite/uart', str(2+len(args_str)), args_str])
